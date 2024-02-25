9e9c7b564bf8412af5ca9dcf307844e4b2578652
Zack Hanni <zack.hannI@gmail.com>
2024-02-25 16:41:10 -0500

    replace init with demo components

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Biography from "./components/Biography";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Listen from "./components/Listen";
+import Nav from "./components/Nav";
+// import Photos from "./components/Photos";
+import Videos from "./components/Videos";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="App bg-black">
+      <header className="App-header">
+        <Nav />
+        <main className="pt-16">
+          <Hero />
+          {/* <Photos /> */}
+          <Biography />
+          <Listen />
+          <Videos />
+        </main>
 
-export default App
+        <Footer />
+      </header>
+    </div>
+  );
+}
