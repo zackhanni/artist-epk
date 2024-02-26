e466591f73f59bfc671e82f6616ab8133b2121c6
Zack Hanni <zack.hannI@gmail.com>
2024-02-25 22:25:08 -0500

    reorder nav links. fix bg color

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,19 +2,19 @@ import { artistBio } from "./ArtistInformation";
 
 export default function Nav() {
   const navItems = [
+    { name: "Photos", link: "#photos" },
     { name: "Biography", link: "#biography" },
     { name: "Listen", link: "#listen" },
     { name: "Videos", link: "#videos" },
-    // { name: "Shows", link: "#shows" },
   ];
 
   return (
-    <nav className="h-16 w-full fixed top-0 bg-white/10 text-white backdrop-blur-sm flex justify-between px-8 items-center">
+    <nav className="h-16 w-full fixed top-0 bg-black/10 text-white backdrop-blur-sm flex justify-between px-8 items-center">
       <h1 className="">{artistBio.name} EPK</h1>
       <div className="space-x-3">
         {navItems.map((item) => {
           return (
-            <a href={item.link} className="hover:underline">
+            <a key={item.name} href={item.link} className="hover:underline">
               {item.name}
             </a>
           );
