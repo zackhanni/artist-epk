ce1bed0538d1f7b1ec8df2f3c6475210931a4da8
Zack Hanni <zack.hannI@gmail.com>
2024-02-25 22:33:27 -0500

    change from grid to flex

--- src/components/Videos.tsx
+++ src/components/Videos.tsx
@@ -2,15 +2,18 @@ import { videoLinks } from "./ArtistInformation";
 
 export default function Videos() {
   return (
-    <section className="flex flex-col justify-center space-y-16 py-16 bg-[#f0ece2] text-[#596e79] ">
+    <section className="py-16 bg-[#f0ece2] text-[#596e79] ">
       <h2 id="videos" className=" text-4xl text-center">
         Videos
       </h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 gap-5 max-w-screen-md mx-auto">
+      <div className="flex flex-row flex-wrap items-center justify-center pt-16  max-w-screen-md mx-auto  ">
         {videoLinks.map((video) => {
           return (
-            <div key={video.title} className="flex flex-col">
-              <h3 className=" text-center text-2xl">{video.title}</h3>
+            <div
+              key={video.title}
+              className="flex flex-col justify-center items-center p-4"
+            >
+              <h3 className=" text-center text-xl pb-2">{video.title}</h3>
               <div className="mx-auto rounded-xl overflow-hidden shadow-2xl">
                 <iframe
                   className=" h-[200px] w-auto"
