7b14bd30644b23fec5768a4145f217c3c5242bfa
Zack Hanni <zack.hannI@gmail.com>
2024-02-25 22:35:55 -0500

    use mt to compensate for hover:border

--- src/components/Listen.tsx
+++ src/components/Listen.tsx
@@ -3,21 +3,16 @@ import { artistBio, streamingLinks, albums } from "./ArtistInformation";
 export default function Listen() {
   return (
     <section id="listen" className="text-white py-16 max-w-screen-md mx-auto">
-      <div>
-        <h2 className="text-4xl text-center">
-          Listen to {artistBio.name} on all streaming platforms.
-        </h2>
-      </div>
-
+      <h2 className="text-4xl text-center">
+        Listen to {artistBio.name} on all streaming platforms.
+      </h2>
       <div className="flex flex-row items-center justify-center space-x-4 py-8 ">
         {streamingLinks.map((platform) => {
           return (
-            <div key={platform.name} className="">
-              <a href={platform.link} className="text-center">
-                {/* <h3>{platform.name}</h3> */}
-                {platform.icon}
-              </a>
-            </div>
+            <a key={platform.name} href={platform.link} className="text-center">
+              {/* <h3>{platform.name}</h3> */}
+              {platform.icon}
+            </a>
           );
         })}
       </div>
@@ -30,7 +25,7 @@ export default function Listen() {
                 href={album.link}
                 key={album.name}
                 className="mx-auto bg-blue-800 p-4 rounded-3xl hover:-translate-y-2 hover:-translate-x-2 duration-300 
-             hover:border-b-8 hover:border-r-8 border-blue-700 box-border"
+             hover:border-b-8 hover:border-r-8 border-blue-600 hover:-mt-2 box-border"
               >
                 <img
                   src={album.image}
